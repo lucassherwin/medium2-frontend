f9ac0e1ec05ada9116b886ad608208c64d7f32fd
lucassherwin <lksherwin@gmail.com>
2020-08-19 12:19:13 -0400

    changes

--- src/Components/login.js
+++ src/Components/login.js
@@ -1,8 +1,8 @@
 import React, { Component } from 'react'
 
 export class login extends Component {
-    stae = {
-        username: '',
+    state = {
+        username: "",
         password: ''
     }
 
@@ -26,7 +26,7 @@ export class login extends Component {
             <div>
                 <p>Login</p>
                 <form>
-                    Username: <input type='text' placeholder='username' onChange={this.handleUsername}></input> <br />
+                    Username: <input type='text' placeholder='username' onChange={(event) => this.handleUsername(event)}></input> <br />
                     Password: <input type='password' placeholder='password' onChange={this.handlePassword}></input>
                     <button onChange={(event) => this.handleSumbit(event)}>Submit</button>
                 </form>
